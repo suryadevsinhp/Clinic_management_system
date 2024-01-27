6e10e9c6a8b75ac130c80bf7189cbf1907bc6ce0
suryadevsinhp <patilsurya111@gmail.com>
2024-01-27 18:03:51 +0530

    css added to some templates

--- hems_dental/hems_dental/settings.py
+++ hems_dental/hems_dental/settings.py
@@ -39,8 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'data_entry','clinic','phonenumber_field'
+    'data_entry','clinic','phonenumber_field',
+    # below two lines are added for bootstrap
+    "crispy_forms",
+    "crispy_bootstrap5", 
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
